f4e4b84d5464fed325138937fd42096aa68cef3b
Kaczupinko <119810971+Kaczupinko@users.noreply.github.com>
2023-03-29 14:14:56 +0100

    upload

--- Exercises.js
+++ Exercises.js
@@ -38,7 +38,7 @@ function canMarry(age, gender, maritalStatus) {
 }
 
 //  console.log(canMarry(21, 'female', 'single')); 
-
+/*
 let x = 10;
 x += 5;
 let y = 20;
@@ -109,3 +109,137 @@ function checkTriangleType(a, b, c) {
 //console.log(checkTriangleType(1, 1, 1));
 //console.log(checkTriangleType(1, 1, 2));
 //console.log(checkTriangleType(1, 2, 3));
+
+//hoisting - przsyniecie deklaracji na poczatek kodu 
+x = 10;
+console.log(x);
+var x;
+
+foo();               //hoisting jest to przesnoszenie deklarancji funkcji na poczatek kodu
+function foo() {
+    console.log('foo');
+}   //hoisting function expression
+*/
+// poprawianie bledow 30 min 
+/*
+function calculateAverage(numbers) {
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 0; i < numbers.length; i++) { //brak nawiasow klamrowych
+        sum += numbers[i];
+        count++;
+    }
+
+    return sum / count;
+}
+
+let numbers = [2, 4, 6, 8];
+let result = calculateAverage(numbers);
+console.log("The average is: " + result);
+*/
+/*
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+  }
+
+  
+let first = "John";
+let last = "Doe";
+let name = getFullName(firstName, lastName); 
+console.log("The full name is: " + name);
+*/
+/*
+function sumEvenNumbers(numbers) {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+
+        if (number % 2 === 0) {
+            sum = sum + number;
+        }
+    }
+
+    return sum;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6];
+let result = sumEvenNumbers(numbers);
+console.log("The sum of even numbers is: " + result);
+*/
+/*
+function uniqueNumbers(numbers) {
+    let unique = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (unique.indexOf(numbers[i]) === -1) {
+            unique.push(numbers[i]);
+        }
+    }
+
+    return unique;
+}
+
+let numbers = [1, 2, 3, 2, 4, 3, 5];
+let result = uniqueNumbers(numbers);
+console.log("Unique numbers are: " + result);
+*/
+/*
+function calculateAverage(numbers) {
+    let sum = 0;
+
+    if (numbers.length > 0) {
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        let average = sum / numbers.length;
+        return average;
+    } else {
+        return 0;
+    }
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let result = calculateAverage(numbers);
+console.log("The average is: " + result);
+
+*/
+/*
+function multiplyBy2(numbers) {
+    let newArray = numbers.slice();
+
+    for (let i = 0; i < newArray.length; i++) {
+        newArray[i] = newArray[i] * 2;
+    }
+
+    return newArray;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let result = multiplyBy2(numbers);
+console.log("The result is: " + result);
+*/
+
+function oddNumbers(input) {
+    let newArray = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] % 2 === 1) {
+            newArray.push(input[i])
+        }
+    }
+
+    return newArray;
+};
+console.log(oddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+const singleQuotes = 'single quotes\n and new line';
+const doubleQuotes = "double quotes\n and new line";
+const backticks = `backticks 
+and new 
+line`;
+console.log(singleQuotes);
+console.log(doubleQuotes);
+console.log(backticks);
